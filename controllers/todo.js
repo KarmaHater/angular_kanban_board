f2ee7300e1f3055603eaa01827bef73ba5351cc2
Adnard-kod <andra.lally@gmail.com>
2014-12-16 12:42:11 -0800

    added drag and drop function

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,21 +1,14 @@
 angular.module('kanbanBoard')
 .controller('TodoCtrl', function ($scope) {
-  $scope.tasks = [
-    { task:"eiko", status: "Todo"},
-    { task:"eiko1", status: "Working On"},
-    { task:"eiko2", status: "Done"}];
-  $scope.data = {
-    lists: [
-      {label: "Todo"},
-      {label: "Working On"},
-      {label: "Done"}
-    ]};
+  $scope.tasks = [];
    $scope.addTodo = function (todo) {
       $scope.tasks.push({task: todo, status: "Todo"})
       $scope.newTodo = ""
-    },
-    $scope.moveTodo = function () {
-      alert("hello")
-      // $('.todo').draggable({ containment: 'document', revert: true})
-    }
+    };
+    $scope.dropSuccessHandler = function($event, item){
+      item.status = $scope.newLocation
+    };
+    $scope.onDrop = function($event,$data, location){
+      $scope.newLocation = location
+    };
 })
